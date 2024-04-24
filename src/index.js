2e19375af16a3b05a5096569c5fca155da3628ae
Adityak <adityakolekar47@gmail.com>
2024-04-24 12:02:23 +0530

    done with ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
+import { store } from "./redux/Store";
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+            <Toaster />
+        </Provider>
+    </BrowserRouter>
 );
